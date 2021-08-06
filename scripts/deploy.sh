3f8055fc49bd76797d60cffc958a6ba6a52d3c58
Sven Vowe <svenvowe@gmail.com>
2021-08-06 16:22:53 +0200

    prepared for deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+echo "Deploying..."
+
+ssh -T $DEPLOYMENT_USERNAME@$DEPLOYMENT_HOST << EOSSH
+cd $DEPLOYMENT_PATH
+git pull
+npm install
+EOSSH
+
+#cd $DEPLOYMENT_ECOSYSTEM_PATH
+#pm2 restart ecosystem.config.js
