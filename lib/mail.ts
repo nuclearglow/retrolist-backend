411d598655a2fddd3927291d45ee019e6a7eb629
Sven Vowe <svenvowe@gmail.com>
2021-08-07 12:11:22 +0200

    ignoretls only for dev

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,7 +5,7 @@ import { createTransport } from 'nodemailer';
 const transport = createTransport({
     host: process.env.MAIL_HOST,
     port: Number(process.env.MAIL_PORT),
-    ignoreTLS: true,
+    ignoreTLS: process.env.NODE_ENV !== 'production',
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
